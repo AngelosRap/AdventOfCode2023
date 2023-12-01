04ddeda55c4672e80d458a0bfd61271c24655fc0
DESKTOP-1F7MI45\lala <aggelos.rapt@gmail.com>
2023-12-02 01:05:41 +0200

    3ana_gamiese

--- AdventOfCode2023/AdventOfCode2023/Day_1.cs
+++ AdventOfCode2023/AdventOfCode2023/Day_1.cs
@@ -1,6 +1,7 @@
 ﻿using AdventOfCode2023.Utilities;
 
 namespace AdventOfCode2023;
+
 public enum Nums
 {
     zero, one, two, three, four, five, six, seven, eight, nine
@@ -10,19 +11,44 @@ public class Day1
     public static void Part2()
     {
         var lines = Helpers.GetLines();
-        var solution = 0;
+        List<string> modifiedLines = new();
 
         foreach (var line in lines)
         {
+            Dictionary<int, int> numIndexes = new();
+            foreach (var item in Enum.GetValues(typeof(Nums)))
+            {
+                if (line.IndexOf(item.ToString()) < 0)
+                {
+                    continue;
+                }
 
+                numIndexes.Add(line.IndexOf(item.ToString()), (int)item);
+            }
+
+            var sorted = numIndexes.OrderBy(x => x.Key);
+            var newLine = line;
+            if (sorted.Count() > 0)
+            {
+                newLine = line.Replace(Enum.GetName(typeof(Nums), (Nums)sorted.ElementAt(0).Value), sorted.ElementAt(0).Value.ToString()).Replace(Enum.GetName(typeof(Nums), (Nums)sorted.ElementAt(^1).Value), sorted.ElementAt(^1).Value.ToString());
+            }
+
+            modifiedLines.Add(newLine);
         }
+        Console.WriteLine(Solution(modifiedLines));
+
     }
     public static void Part1()
     {
         var lines = Helpers.GetLines();
-        var solution = 0;
 
-        foreach (var line in lines)
+        Console.WriteLine(Solution(lines));
+    }
+
+    private static int Solution(List<string> strings)
+    {
+        var solution = 0;
+        foreach (var line in strings)
         {
             var charList = new List<char>();
             string stringSum;
@@ -42,8 +68,9 @@ public class Day1
                 stringSum = charList[0].ToString() + charList[^1].ToString();
             }
             solution += int.Parse(stringSum);
+            Console.WriteLine(stringSum);
         }
-        Console.WriteLine(solution);
+        return solution;
     }
 
 
